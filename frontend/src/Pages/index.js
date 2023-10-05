31ce948110e28aa25ee4fef0d4b3b17cce879857
yashcrest <yashcrest@gmail.com>
2023-10-06 10:23:38 +1030

    server logic

--- frontend/src/Pages/index.js
+++ frontend/src/Pages/index.js
@@ -3,3 +3,5 @@ export { default as Cart } from "./Cart";
 export { default as Product } from "./Product";
 export { default as PageNotFound } from "./PageNotFound";
 export { default as Checkout } from "./Checkout";
+export { default as Success } from "./Success";
+export { default as Failed } from "./Failed";
