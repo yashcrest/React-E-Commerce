b83928231b73341129338dbbd4f974767073576b
Yash Shrestha <79971012+yashcrest@users.noreply.github.com>
2023-11-06 19:08:01 +1030

    removing trailing "/" from the baseURL

--- frontend/src/Pages/Cart.jsx
+++ frontend/src/Pages/Cart.jsx
@@ -46,7 +46,7 @@ const Cart = () => {
 
   //axios backend call
   const backendClient = axios.create({
-    baseURL: "https://react-e-commerce-backend.vercel.app/",
+    baseURL: "https://react-e-commerce-backend.vercel.app",
   });
 
   //calling backend
